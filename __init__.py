0723abff6f286418db4a8ac6578406cdb6d515d3
Shubhangi Goyal <shubhangi_goyal@Shubhangis-MacBook-Pro.local>
2022-10-09 23:00:58 +0530

    initilize the project

---
+++ __init__.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import cv2,os,math
+import matplotlib
+
+class NNDetection:
+    def __init__(self, data):
+        self.data =  data
+
+
+if __name__ == '__main__':
+    
